90bcb7959cc698e34221ad91212e6a3c21274384
hanine <hanine@macbook-air.home>
2023-10-03 20:39:59 +0200

    add include

---
+++ include/ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanjebou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 15:47:56 by hanjebou          #+#    #+#             */
+/*   Updated: 2023/10/03 15:56:27 by hanjebou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# define FORMAT "cspdiuxX%"
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdarg.h>
+# include <limits.h>
+
+#endif //FT_PRINTF_H 
